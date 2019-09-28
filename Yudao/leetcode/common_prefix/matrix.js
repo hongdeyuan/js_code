d43a006212d790b5a8464dc0cd8018ca821fc69f
hongdeyuan <1737407069@qq.com>
2019-09-28 08:32:42 +0800

    feat：9月27日前端练习

--- Yudao/leetcode/common_prefix/matrix.js
+++ Yudao/leetcode/common_prefix/matrix.js
@@ -4,28 +4,29 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    if (matrix.length < 1 || matrix[0].length === 0 || matrix[0][0] > target) {
-        return false
-    } else {
-
-        for (var i = 0; i < matrix.length; i++) {
-            while (j < matrix.length) {
-                if (matrix[i][j] === target) {
-                    return true;
-                }
-                j++;
-            }
-        }
+    if (matrix.length === 0 || matrix[0].length === 0 || matrix[0][0] > target)
         return false;
+    var i = matrix.length - 1,
+        j = 0;
+    while (i >= 0 && j < matrix[0].length) {
+        if (matrix[i][j] === target)
+            return true;
+        if (matrix[i][j] < target) {
+            j += 1;
+        } else {
+            i -= 1;
+        }
     }
+    return false;
 };
+
 
 const matrix = [
     [1, 4, 7, 11, 15],
     [2, 5, 8, 12, 19],
     [3, 6, 9, 16, 22],
-    [10, 13, 14, 17, 24],
-    [18, 21, 23, 26, 30]
+    [11, 13, 14, 17, 24],
+    [18, 20, 23, 26, 30]
 ];
 const target = 5;
 console.log(searchMatrix(matrix, target));
